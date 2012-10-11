3540ccf426ba60f419b38489b1e0975ba0a0836d
Erik Cunningham <djtrinary@gmail.com>
2012-10-11 00:24:59 -0600

    would this be better in perl?  Dunno.

--- data_sources/loadavg.sh
+++ data_sources/loadavg.sh
@@ -2,6 +2,6 @@
 while true ; do
   sleep 3
   t="`date +%s`000"
-  l=`uptime | uptime | cut -d":" -f3 | cut -d' ' -f2`
+  l=`uptime | ruby -ne 'puts $_.split(" ")[-2]'`
   curl -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://localhost:3000/data
 done
