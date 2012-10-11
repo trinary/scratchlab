83a4ba5171839613da8c1048a10523ca3f0aae60
Erik Cunningham <djtrinary@gmail.com>
2012-10-10 23:46:22 -0600

    how is there no more convenient way to get the load average than this?

--- data_sources/loadavg.sh
+++ data_sources/loadavg.sh
@@ -2,7 +2,6 @@
 while true ; do
   sleep 3
   t="`date +%s`000"
-  l=`uptime | awk '{print $10}'`
+  l=`uptime | uptime | cut -d":" -f3 | cut -d' ' -f2`
   curl -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://localhost:3000/data
-  echo
 done
