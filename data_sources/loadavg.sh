de0c245f348dd115eb3f678a54a71a8bea99e82c
Erik Cunningham <djtrinary@gmail.com>
2012-10-18 16:45:40 -0600

    slow down a little, add .git to nodemonignore

--- data_sources/loadavg.sh
+++ data_sources/loadavg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true ; do
-#  sleep 0.1
+  sleep 0.5
   t=`ruby -e 'puts (Time.now.to_f * 1000).to_i'`
   l=`uptime | ruby -ne 'puts $_.split(" ")[-2]'`
   curl -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://127.0.0.1:3000/data
