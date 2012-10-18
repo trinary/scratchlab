b25903f03bd79392e281149de4fd8fba1705c058
Erik Cunningham <djtrinary@gmail.com>
2012-10-18 15:59:30 -0600

    don't send curl -v output

--- data_sources/loadavg.sh
+++ data_sources/loadavg.sh
@@ -3,5 +3,5 @@ while true ; do
   sleep 0.1
   t=`ruby -e 'puts (Time.now.to_f * 1000).to_i'`
   l=`uptime | ruby -ne 'puts $_.split(" ")[-2]'`
-  curl -v -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://127.0.0.1:3000/data
+  curl -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://127.0.0.1:3000/data
 done
