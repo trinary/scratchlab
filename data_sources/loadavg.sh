38df536484452eddb5e35e4e4f435705351f5837
Erik Cunningham <djtrinary@gmail.com>
2012-10-18 15:42:52 -0600

    milisecond precision, sleep 0.1, use 127.0.0.1:3000

--- data_sources/loadavg.sh
+++ data_sources/loadavg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true ; do
-  sleep 3
-  t="`date +%s`000"
+  sleep 0.1
+  t=`ruby -e 'puts (Time.now.to_f * 1000).to_i'`
   l=`uptime | ruby -ne 'puts $_.split(" ")[-2]'`
-  curl -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://localhost:3000/data
+  curl -v -X POST -d "{\"type\":\"loadavg\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' http://127.0.0.1:3000/data
 done
