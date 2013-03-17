2728708b1ea64e51654897f15af08455b52534e8
Erik Cunningham <djtrinary@gmail.com>
2013-03-17 15:48:23 -0600

    random xy scatter data

---
+++ data_sources/xy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+while true ; do
+  sleep 1.5
+  t=`ruby -e 'puts (Time.now.to_f * 1000).to_i'`
+  x=$RANDOM
+  y=$RANDOM
+  curl -X POST -d "{\"type\":\"scatter\", \"name\":\"random\", \"x\": $x, \"y\": $y}" -H 'Content-Type: application/json' http://127.0.0.1:3000/data
+done
