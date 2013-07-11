e48cc9ea542e9e3718695360a0aa0d1fa20d9502
Erik Cunningham <djtrinary@gmail.com>
2013-07-11 14:19:39 -0600

    fix the rest of the data sources with keys

--- data_sources/random.sh
+++ data_sources/random.sh
@@ -3,5 +3,5 @@ while true ; do
   sleep 1.5
   t="`date +%s`000"
   l=$RANDOM
-  curl -u "example: " -X POST -d "{\"type\":\"timeline\", \"name\":\"random\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' $1
+  curl -u "$2:" -X POST -d "{\"type\":\"timeline\", \"name\":\"random\", \"value\": $l, \"timestamp\": $t}" -H 'Content-Type: application/json' $1
 done
